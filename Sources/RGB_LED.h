239aad86354dbf0741cb7d629d02db59814c7b6f
gerads <pdgerads@gmail.com>
2015-12-05 17:31:38 -0600

    remove old demo methods
    Don't need these since they're in Git now.

--- Sources/RGB_LED.h
+++ Sources/RGB_LED.h
@@ -9,8 +9,5 @@
 #define RGB_LED_H_
 
 void makeColor(unsigned int color);
-//void makeColor(int red, int green, int blue);
-//void colorWheel(void);
-//void breathingColor(int red, int green, int blue, int time, int repetitions);
 
 #endif /* RGB_LED_H_ */
